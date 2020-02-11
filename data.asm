e4f4336d2c37109094b0dc2dbc2c532be1180df0
SoniGarima <2018csb1089@iitrpr.ac.in>
2020-02-11 21:24:02 +0530

    addi,andi,ori,sb,sw,sh,sd DONE

---
+++ data.asm
@@ -0,0 +1,7 @@
+sb x1,0(x2)
+addi x3,x2,-89
+andi x4,x5,23
+ori x4,x8,34
+sd x1,0(x2)
+sw x1,0(x2)
+sh x1,0(x2)
--- z1.py
+++ z1.py
@@ -4,8 +4,8 @@ os.remove("coded.mc")
 f2=open("coded.mc","x")
 lines=f1.readlines()
 for line in lines:
-    string,rem=line.split();
-    if string=="addi" or "andi" or "ori":
+    string,rem=line.split()
+    if (string=="addi" or string=="andi" or string=="ori"):
         k1,k2,k3=rem.split(",")
         h1=str(bin(int(k1[1],10))[2:].zfill(5))
         h2=str(bin(int(k2[1],10))[2:].zfill(5))
@@ -34,6 +34,36 @@ for line in lines:
         mac_codet=h3+h2+func3+h1+opc
         mac_code=str(hex(int(mac_codet,2))[2:].zfill(8))
         f2.write("0x"+mac_code+"\n")
-        
-
+    elif (string=="sw" or string=="sb" or string=="sd" or string=="sh"):
+        k1,k2=rem.split(",")
+        k3,reg=k2.split("(")
+        h1=str(bin(int(k1[1],10))[2:].zfill(5)) 
+        h2= str(bin(int(reg[1],10))[2:].zfill(5)) 
+        if(int(k3)>=0):
+            if(k3.startswith("0x")):
+                h3=str(bin(int(k3, 16))[2:].zfill(12))
+            else:    
+                h3=str(bin(int(k3,10))[2:].zfill(12)) 
+        elif(int(k3)<0):
+            if(k3.startswith("0x")):
+                h3=str(bin(int(k3, 16))[3:].zfill(12))
+            else:    
+                h3=str(bin(int(k3,10))[3:].zfill(12)) 
+            k3=int(h3,2)
+            k3=2**12-k3 
+            k3=str(k3)    
+            h3=str(bin(int(k3,10))[2:].zfill(12)) 
+        if(string=="sw"):
+            func3="010"
+                
+        elif(string=="sd"):
+            func3="011"
+        elif(string=="sh"):
+            func3="001"
+        else: 
+            func3="000"  
+        opc="0100011"     
+        mac_codet=h3[0:7]+h1+h2+func3+h3[7:]+opc
+        mac_code=str(hex(int(mac_codet,2))[2:].zfill(8))
+        f2.write("0x"+mac_code+"\n")
  
