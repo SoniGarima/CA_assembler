32ff40818f4da72672a6ffb574331faf741768a7
SoniGarima <2018csb1089@iitrpr.ac.in>
2020-02-11 19:01:44 +0530

    Signed part done

--- z1.py
+++ z1.py
@@ -9,10 +9,21 @@ for line in lines:
         k1,k2,k3=rem.split(",")
         h1=str(bin(int(k1[1],10))[2:].zfill(5))
         h2=str(bin(int(k2[1],10))[2:].zfill(5))
-        if(k3.startswith("0x")):
-            h3=str(bin(int(k3, 16))[2:].zfill(12))
-        else:    
-            h3=str(bin(int(k3,10))[2:].zfill(12))
+        if(int(k3)>0):
+            if(k3.startswith("0x")):
+                h3=str(bin(int(k3, 16))[2:].zfill(12))
+            else:    
+                h3=str(bin(int(k3,10))[2:].zfill(12)) 
+        elif(int(k3)<0):
+            if(k3.startswith("0x")):
+                h3=str(bin(int(k3, 16))[3:].zfill(12))
+            else:    
+                h3=str(bin(int(k3,10))[3:].zfill(12)) 
+            k3=int(h3,2)
+            k3=2**12-k3; 
+            k3=str(k3)    
+            h3=str(bin(int(k3,10))[2:].zfill(12)) 
+            print(h3)              
         if(string=="addi"):
             func3="000"
         elif( string=="andi"):
