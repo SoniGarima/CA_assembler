861eb1aaa9519ed2ace7b58d22d95a1a1109ad31
Anamika-11 <2018csb1072@iitrpr.ac.in>
2020-03-07 17:55:00 +0530

    beq,bne,blt,bge

--- z2.py
+++ z2.py
@@ -1,7 +1,7 @@
 import os
 f1=open("data1.asm")
-os.remove("coded.mc")
-f2=open("coded.mc","x")
+os.remove("coded1.mc")
+f2=open("coded1.mc","x")
 lines=f1.readlines()
 count1=0
 for line in lines:
@@ -27,13 +27,11 @@ for line in lines:
                 count2=(count2-count1)-1
             else:
                 count2=(count2-count1)+1
-            print(count2)
             imm0=4*count2
             if(imm0>=0):
                 imm=str(bin(imm0)[2:].zfill(12)) 
             else:  
                 imm=str(bin(imm0)[3:].zfill(12)) 
-                print(imm)
                 imm0=int(imm,2)
                 imm0=2**12-imm0
                 imm0=str(imm0)    
@@ -52,7 +50,6 @@ for line in lines:
             opcode="1100011"
             mac_codet=imm1+h2+h1+func3+imm2+opcode
             mac_code=str(hex(int(mac_codet,2))[2:].zfill(8))
-            print(mac_codet)
             f2.write("0x"+mac_code+"\n")
         
 
