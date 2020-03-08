a562ec9b6cca4deafc7ab9247f0b4cf11f0ee0e5
sweety879 <58600978+sweety879@users.noreply.github.com>
2020-03-08 14:10:16 +0530

    Update z5.py

--- z5.py
+++ z5.py
@@ -8,42 +8,6 @@ lines=f1.readlines()
 for line in lines:
     str1,str2=line.split(" ")
     k1,k2,k3=str2.split(",")
-    h1=str(bin(int(k1[1],10))[2:]).zfill(5)
-    h2=str(bin(int(k2[1],10))[2:]).zfill(5)
-    h3=str(bin(int(k3[1],10))[2:]).zfill(5)
-    opc="0100011"
-    if(str1=="sra" or str1=="sub"):
-        fun7="0010000"
-    elif(str1=="mul" or str1=="div" or str2=="rem"):
-        fun7="0000001"
-    else:
-        fun7="0000000"
-    if(str1=="add" or str1=="sub" or str1=="mul"):
-        fun3="000"
-    elif(str1=="rem" or str2=="or"):
-        fun3="110"
-    elif(str1=="sra" or str2=="srl"):
-        fun3="101"
-    elif(str1=="div" or str1=="xor"):
-        fun3="100"
-    elif(str1=="and"):
-        fun3="111"
-    elif(str1=="sll"):
-        fun3="001"
-    elif(str1=="slt"):
-        fun3="010"
-    mac_codet=fun7+h1+h2+h3+fun3+opc
-    mac_code=str(hex(int(mac_codet,2))[2:].zfill(8))
-    f2.write("0x"+mac_code+"\n")
-    
-
-
-# In[30]:
-
-lines=["or x3,x2,x1","rem x3,x2,x1"]
-for line in lines:
-    str1,str2=line.split(" ")
-    k1,k2,k3=str2.split(",")
     h3=str(bin(int(k1[1],10))[2:]).zfill(5)
     h2=str(bin(int(k2[1],10))[2:]).zfill(5)
     h1=str(bin(int(k3[1],10))[2:]).zfill(5)
@@ -73,9 +37,5 @@ for line in lines:
     #f2.write("0x"+mac_code+"\n")
     f2=str("0x"+mac_code+"\n")
     print(line+"="+f2)
-
-
-# In[ ]:
-
 
 
