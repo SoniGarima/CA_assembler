895359d99eac8e70cae3aaf03ce4ae325a0fcca7
Garima Soni <2018csb1089@iitrpr.ac.in>
2020-03-11 22:33:12 +0530

    updated

--- registers.py
+++ registers.py
@@ -1,6 +1,18 @@
-registers=[]
-for i in range(0,32):
-    registers[i]=0
-registers[0]=X0
-registers[2]=
-registers[3]=    
+class registers:
+    def __init__(self):
+        self.reg=[]
+        for i in range(0,32):
+            reg[i]=0
+        reg[2]=2147483632
+        reg[3]=268435456
+    def regVal(self,bin_str) :
+        return self.reg[int(bin_str,2)]  
+    
+    def write_reg(self,bin_str,val):
+        if(bin_str=="00000"):
+            return  
+        else:
+            self.reg[int(bin_str,2)]=val
+            
+    def ret_reg(self):
+        return self.registers
